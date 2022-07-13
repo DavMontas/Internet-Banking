36133110af81f1a12332093c402c8316800f3d7f
Dav <davmontmontas@gmail.com>
2022-07-12 22:21:27 -0400

    config test

--- InternetBanking/Startup.cs
+++ InternetBanking/Startup.cs
@@ -26,6 +26,7 @@ namespace InternetBanking
 
         public IConfiguration Configuration { get; }
 
+        //adding gitignore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
