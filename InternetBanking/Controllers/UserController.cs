94f8d52dc1a2ba1f467ff1309ce35311cf1d39e7
Ronald Brito <britoskies@gmail.com>
2022-07-13 20:47:53 -0400

    Designed login view with controller

---
+++ InternetBanking/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.InternetBanking.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
--- InternetBanking/Startup.cs
+++ InternetBanking/Startup.cs
@@ -70,7 +70,7 @@ namespace InternetBanking
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=User}/{action=Login}/{id?}");
             });
         }
     }
